896dedfc8bb0befdacfce1dd3190cffb9b7d43d4
smartgreendeer <johnmuriithi7818@gmail.com>
2024-06-22 14:41:29 +0300

    api

--- hello_world.py
+++ hello_world.py
@@ -6,7 +6,7 @@ async def main():
     # Load the flow from the file
     flow = AsyncFlows.from_file("hello_world.yaml")
 
-    MY_NAME= "John"
+    MY_NAME = "John"
     # Run the flow
     result = await flow.set_vars(name="MY_NAME").run()
 
